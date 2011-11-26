37c38b00dae18a1c907b439a3a130a6feb6af51d
Marco Pivetta <ocramius@gmail.com>
2011-11-26 09:02:23 +0100

    Fixing typo More specific autoloading for Symfony components

--- autoload_register.php
+++ autoload_register.php
@@ -3,14 +3,14 @@ spl_autoload_register(include __DIR__ . '/autoload_function.php');
 Zend\Loader\AutoloaderFactory::factory(array(
     'Zend\Loader\StandardAutoloader' => array(
         Zend\Loader\StandardAutoloader::LOAD_NS => array(
-            /** @todo fix autoloading here */
             'ZfPhpcrOdm'                => __DIR__ . '/src/ZfPhpcrOdm',
             'Doctrine\ODM'              => __DIR__ . '/library/doctrine-phpcr-odm/lib/Doctrine/ODM',
             'Doctrine\Common'           => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/doctrine-common/lib/Doctrine/Common',
             'Jackalope'                 => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/jackalope/src/Jackalope',
-            'PHPCR\Util'                => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/jackalope/lib/phpcr-util/src/PHPCR/Util',
+            'PHPCR\Util'                => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/jackalope/lib/phpcr-utils/src/PHPCR/Util',
             'PHPCR'                     => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/jackalope/lib/phpcr/src/PHPCR',
-            'Symfony'                   => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/Symfony',
+            'Symfony\Component\Yaml'    => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/Symfony/Component/Yaml',
+            'Symfony\Component\Console' => __DIR__ . '/library/doctrine-phpcr-odm/lib/vendor/jackalope/lib/phpcr-utils/lib/vendor/Symfony/Component/Console',
         ),
     ),
 ));
